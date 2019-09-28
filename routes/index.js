6beed161dcc9371ff18141a4e2bcdc7aad969596
sinan.tok <sinan.tok@demirorenteknoloji.com>
2019-09-29 00:43:50 +0300

    encrypting with bcrypt and password validation edited

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,24 @@ router.get('/', (req, res, next) => {
 router.post('/register', (req, res, next) => {
 
   const { userName, password } = req.body;
-  const user = new UserModel({userName, password});
-  const promise = user.save();
-  promise.then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
-  });  
+
+  if( password.length > 15 || password.length < 6 ){
+    res.json("password must be greater than 6 character or less than 15 character");
+    return false;
+  }
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new UserModel({
+      userName, 
+      password: hash
+    });
+
+    const promise = user.save();
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });  
+  });
 
 })
 
